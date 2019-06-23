315924f1b63d7f4f825cf124bd03da4f15c54448
Seetha Rama Krushna Raju <rajukrushna@live.com>
2019-06-23 17:23:53 +0530

    removed sqlite db and added mysqldb in settings.py

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'bookstore_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bookstore',
+        'USER': 'bookstoreadmin',
+        'PASSWORD': 'Book.admin69',
+        'HOST': '103.74.54.23',
+        'PORT': '3306',
     }
 }
 
