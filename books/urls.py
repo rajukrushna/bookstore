812ab33afafbf34b6a69d9b9d434bc938291837d
Seetha Rama Krushna Raju <rajukrushna@live.com>
2019-06-23 16:56:44 +0530

    added schema to the api at /schema

--- books/urls.py
+++ books/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from rest_framework.schemas import get_schema_view
 
+schema_view = get_schema_view(title='Bookstore API')
 
 router = DefaultRouter()
 router.register(r'books', views.BookViewSet)
 
 urlpatterns = [
+    path('schema/', schema_view),
     path('', include(router.urls)),
 ]
